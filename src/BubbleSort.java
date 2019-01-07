16b7ce87d22e8c8e3a3fe71be0c4b223ec9b2b65
mayanli <ma.yanli@centchain.com>
2019-01-07 14:36:54 +0800

    add sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static Integer[] BubbleSort(Integer arr[]){
+        int len=arr.length;
+        int temp=0;
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++){
+                if(arr[j]<arr[i]){
+                    temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr={2,1,4,3,6};
+        arr=BubbleSort(arr);
+        for(Integer ar:arr){
+            System.out.println(ar);
+        }
+    }
+}
